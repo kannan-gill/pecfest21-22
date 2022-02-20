4cee97929e129dfb3f1ba354cc6c47062c2fc3ed
Kannan Gill <kannangill@gmail.com>
2022-02-20 23:34:47 +0530

    history removed

--- .history/src/Components/AboutPecfest/AboutPecfest_20220220020348.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AboutPecfest() {
-  return (
-    <div>AboutPecfest</div>
-  )
-}
-
-export default AboutPecfest
--- .history/src/Components/AboutPecfest/AboutPecfest_20220220020848.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AboutPecfest() {
-  return (
-    <div className='AboutPecfest'>AboutPecfest</div>
-  )
-}
-
-export default AboutPecfest
